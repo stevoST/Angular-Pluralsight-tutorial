6028bb4a840b57f29a64e26264278af4bb7cc318
Stefan Starosta <stefan.starosta@gmail.com>
2023-02-09 21:15:24 +0100

    getters and setters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,16 @@ export class ProductListComponent implements OnInit{
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = "cart";
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value)
+  }
   products: Iproduct[] = [
     {
       "productId": 1,
@@ -41,6 +50,6 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    console.log('In OnInit')
+    this._glistFilter = 'cart';
   }
 }
