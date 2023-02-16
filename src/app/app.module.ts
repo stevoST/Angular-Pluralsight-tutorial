2bb78e48ff654ebfb4011b7ccbc1cb0cbaf0f66c
Stefan Starosta <stefan.starosta@gmail.com>
2023-02-16 12:34:43 +0100

    tying routes to actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
-import { ProductDetailComponent } from './products/product-detail.component';
+import {ProductDetailComponent} from './products/product-detail.component';
 import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -22,8 +23,16 @@ import {WelcomeComponent} from "./home/welcome.component";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
